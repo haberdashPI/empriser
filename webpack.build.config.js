d4853dd854bba3ac490c5f994748ccaffb18d3d9
David Little <david.frank.little@gmail.com>
2017-07-28 12:35:20 -0400

    Setup variables to optimize react and redux during production build.

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -2,6 +2,12 @@ const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 var config = require('./webpack.config')
 
+config.plugins.push(new webpack.DefinePlugin({
+  'process.env': {
+    NODE_ENV: JSON.stringify('production')
+  }
+}))
+
 config.plugins.push(new webpack.optimize.UglifyJsPlugin({
   compressor: {
     screw_ie8: true,
