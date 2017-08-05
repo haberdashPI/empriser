c439edb5428eca6e8c49a823525b9e6fd33ffda9
David Little <david.frank.little@gmail.com>
2017-08-04 21:01:47 -0400

    Removed spacing between hexes in helper views.

--- src/components/shaders/color_hex.glsl
+++ src/components/shaders/color_hex.glsl
@@ -3,14 +3,14 @@ void color_hex(){
   vec2 wld = wld_shade.xy;
   vec2 axl = wld2axl(wld);
 
-  vec4 n23 = closest_neighbors(axl,wld);
-  vec2 np1 = axl2wld(axl);
-  vec2 np2 = axl2wld(n23.xy);
-  vec2 np3 = axl2wld(n23.zw);
+  // vec4 n23 = closest_neighbors(axl,wld);
+  // vec2 np1 = axl2wld(axl);
+  // vec2 np2 = axl2wld(n23.xy);
+  // vec2 np3 = axl2wld(n23.zw);
 
-  float dist1 = length(wld-np1);
-  float dist2 = length(wld-np2);
-  float dist3 = length(wld-np3);
+  // float dist1 = length(wld-np1);
+  // float dist2 = length(wld-np2);
+  // float dist3 = length(wld-np3);
 
   wld.x = mod(wld.x,map_dims.x);
   if(wld.x < 0.0) wld.x += map_dims.x;
@@ -24,10 +24,10 @@ void color_hex(){
     hex.y = clamp(hex.y,0.0,map_dims.y-1.0);
     gl_FragColor.rgb = hex_color(texture2D(uSampler,hex2dat(hex,filterArea)))*wld_shade.z;
 
-    float width = min(4.0/view_scale,0.1);
-    if(view_scale > 10.0){
-      float edge = min(1.0,min(width,min(dist2 - dist1,dist3 - dist1))/width);
-      gl_FragColor.rgb = mix(gl_FragColor.rgb,vec3(1.0,1.0,1.0),1.0-edge);
-    }
+    // float width = min(4.0/view_scale,0.1);
+    // if(view_scale > 10.0){
+    //   float edge = min(1.0,min(width,min(dist2 - dist1,dist3 - dist1))/width);
+    //   gl_FragColor.rgb = mix(gl_FragColor.rgb,vec3(1.0,1.0,1.0),1.0-edge)*wld_shade.z;
+    // }
   }
 }
